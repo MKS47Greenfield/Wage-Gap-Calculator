b49820774da78be4631c049953e9e894cfdf367b
Christian Straubhaar-Jones <christian.straubhaar@gmail.com>
2016-10-17 11:46:45 -0500

    rendering tabs (super basically)

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,5 +1,7 @@
 var wageGap = angular.module('wageGap', [
   'ui.router',
+  // 'ngMaterial',
+  // 'material.svgAssetsCache',
   'wageGap.sidebar',
   'wageGap.makestatesgraph',
   'wageGap.makebargraph'
@@ -13,18 +15,18 @@ var wageGap = angular.module('wageGap', [
     .state('home', {
       templateUrl: './../index.html',
       url: '/',
-      controller: 'HomeController'
+      controller: 'SidebarController'
     })
-    .state('landing.bargraph', {
+    .state('home.bargraph', {
       templateUrl: './../sidebar/makebargraph.html',
       url: '/bargraph',
       controller: 'MakeBarGraphController'
     })
-    .state('landing.statesgraph', {
+    .state('home.statesgraph', {
       templateUrl: './../sidebar/makestatesgraph.html',
       url: '/statesgraph',
       controller: 'MakeStatesGraphController'
     });
 }).run(function () {
-  
+
 });
